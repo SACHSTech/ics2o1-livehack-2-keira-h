d7993cc21eac41832d11e395ae567b61f61d8e75
Keira Hosey <keira.hosey23@ycdsbk12.ca>
2021-02-23 15:06:35 +0000

    Final Submission

--- problem1.py
+++ problem1.py
@@ -1,1 +1,26 @@
+"""
+----------------------------------------------------------
+Name:    problem1.py
+Purpose: Write a program that tells the user whether they are over or under the given speed limit. If they are over the speed limit, they will be given a fine.
+
+Author:  Hosey.K
+
+Created: date in 23/02/2021
+----------------------------------------------------------
+"""
+
+# Get the information
+spd_limit = float(input("Enter the speed limit: "))
+driver_spd = float(input("Enter the recorded speed of the car: "))
+
+# Compute and Output
+if driver_spd >= spd_limit: 
+  if driver_spd - spd_limit <= 20: 
+    print("You are speeding and your fine is $100")
+  elif driver_spd - spd_limit <= 30:
+    print("You are speeding and your fine is $270")
+  elif driver_spd - spd_limit >= 31:
+    print("You are speeding and your fine is $570")
+else:
+  print("Congratulations, you are within the speed limit!")
 
--- problem2.py
+++ problem2.py
@@ -1,1 +1,23 @@
+"""
+----------------------------------------------------------
+Name:    problem2.py
+Purpose: Write a program to have the user enter three lengths of sides and determine whether the figure is a triangle or not.
 
+Author:  Hosey.K
+
+Created: date in 23/02/2021
+----------------------------------------------------------
+"""
+
+print("----------------- Welcome to the Triangle Checker -----------------")
+
+# Get the side lengths
+side_1 = int(input("Enter the length of side 1: "))
+side_2 = int(input("Enter the length of side 2: "))
+side_3 = int(input("Enter the length of side 3: "))
+
+# Comput results
+if (side_1 + side_2 > side_3) and (side_1 + side_3 > side_2) and (side_2 + side_3 > side_1):
+  print("The figure with the side lengths of " + str(side_1) + "," + str(side_2) + ", and " + str(side_3) + " is a triangle.")
+else:
+  print("The figure with the side lengths of " + str(side_1) + "," + str(side_2) + ", and " + str(side_3) + " is not a triangle.")
